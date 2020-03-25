02bf80448e8b2d84c7c848cad1aa32ca1afc8222
pushthat <matias.berny@icloud.com>
2020-03-25 19:06:06 +0100

    [MOD] sessions management

--- cmd/dbless/dbless.go
+++ cmd/dbless/dbless.go
@@ -2,26 +2,29 @@ package dbless
 
 import (
 	"bytes"
+	"errors"
+	"log"
 
 	"github.com/pushthat/dbless/internal/dblessutils"
 	"github.com/pushthat/dbless/pkg/jsonSerializer"
 	"github.com/pushthat/dbless/pkg/serializer"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
+type DbLessSession struct {
+	S3Bucket  *string
+	Index     *string
+	Prefix    *string
+	S3Session *session.Session
+}
+
 type DbLess struct {
-	S3Bucket       *string
+	DbLessSessions []*DbLessSession
 	SerializerType *string
-	Index          *string
-	Prefix         *string
-	S3Session      *session.Session
 	serializer     serializer.Serializer
-	s3Uploader     *s3manager.Uploader
-	s3Downloader   *s3manager.Downloader
 }
 
 func (dbless *DbLess) Init() error {
@@ -30,54 +33,57 @@ func (dbless *DbLess) Init() error {
 		serializer := jsonSerializer.JsonSerializer{}
 		dbless.serializer = &serializer
 	}
-
-	uploader := s3manager.NewUploader(dbless.S3Session)
-	dbless.s3Uploader = uploader
-	downloader := s3manager.NewDownloader(dbless.S3Session)
-	dbless.s3Downloader = downloader
 	return nil
 }
 
-// TODO error management
 func (dbless *DbLess) Save(obj map[string]interface{}) error {
-	keyName, err := dblessutils.GetObjectIndex(obj, dbless.Index)
-	if err != nil {
-		return err
-	}
+	for _, dblessSession := range dbless.DbLessSessions {
+		keyName, err := dblessutils.GetObjectIndex(obj, dblessSession.Index)
+		if err != nil {
+			return err
+		}
 
-	keyName = *dbless.Prefix + keyName
-	data, err := dbless.serializer.Serialize(obj)
-	if err != nil {
-		return err
-	}
-	upParams := &s3manager.UploadInput{
-		Bucket: dbless.S3Bucket,
-		Key:    &keyName,
-		Body:   bytes.NewReader(data),
-	}
-	_, err = dbless.s3Uploader.Upload(upParams)
-	if err != nil {
-		return err
+		keyName = *dblessSession.Prefix + keyName
+		data, err := dbless.serializer.Serialize(obj)
+		if err != nil {
+			return err
+		}
+		upParams := &s3manager.UploadInput{
+			Bucket: dblessSession.S3Bucket,
+			Key:    &keyName,
+			Body:   bytes.NewReader(data),
+		}
+		err = dblessutils.UploadS3File(upParams, dblessSession.S3Session)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func (dbless *DbLess) Load(obj map[string]interface{}) (map[string]interface{}, error) {
-	keyName, _ := dblessutils.GetObjectIndex(obj, dbless.Index)
-	buf := aws.NewWriteAtBuffer([]byte{})
-	keyName = *dbless.Prefix + keyName
+	for _, dblessSession := range dbless.DbLessSessions {
+		keyName, _ := dblessutils.GetObjectIndex(obj, dblessSession.Index)
+		keyName = *dblessSession.Prefix + keyName
 
-	downloadParam := s3.GetObjectInput{
-		Bucket: dbless.S3Bucket,
-		Key:    &keyName,
-	}
+		downloadParam := s3.GetObjectInput{
+			Bucket: dblessSession.S3Bucket,
+			Key:    &keyName,
+		}
 
-	dbless.s3Downloader.Download(buf, &downloadParam)
-	data, err := dbless.serializer.Deserialize(buf.Bytes())
-	if err != nil {
-		return nil, err
+		bytes, err := dblessutils.GetS3File(&downloadParam, dblessSession.S3Session)
+		if err != nil {
+			log.Println("Error while fectching data : " + err.Error())
+			continue
+		}
+		data, err := dbless.serializer.Deserialize(bytes)
+		if err != nil {
+			log.Println("Error while Deserializing data : " + err.Error())
+			continue
+		}
+		return data, nil
 	}
-	return data, nil
+	return nil, errors.New("Can't fetch data from bucket")
 }
 func (dbless *DbLess) Delete(obj interface{}) {
 
--- internal/dblessutils/dblessutils.go
+++ internal/dblessutils/dblessutils.go
@@ -3,7 +3,29 @@ package dblessutils
 import (
 	"encoding/base64"
 	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
+
+func GetS3File(downloadParam *s3.GetObjectInput, s3Session *session.Session) ([]byte, error) {
+	buf := aws.NewWriteAtBuffer([]byte{})
+
+	downloader := s3manager.NewDownloader(s3Session)
+	downloader.Download(buf, downloadParam)
+	return buf.Bytes(), nil
+}
+
+func UploadS3File(upParams *s3manager.UploadInput, s3Session *session.Session) error {
+	uploader := s3manager.NewUploader(s3Session)
+	_, err := uploader.Upload(upParams)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func GetObjectIndex(object map[string]interface{}, index *string) (string, error) {
 	value, isKey := object[*index]
